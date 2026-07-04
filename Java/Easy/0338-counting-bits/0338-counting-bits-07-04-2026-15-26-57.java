63316bc930204ec4fbf9d4da36b58e481eb54cd8
shahil kumar <kumarshahil3456@gmail.com>
2026-07-04 15:20:10 +0530

    Time: 2 ms (96.54%), Space: 48.5 MB (68.95%) - LeetHub

---
+++ Java/Easy/0338-counting-bits/0338-counting-bits-07-04-2026-15-26-57.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            int temp = i;
+            int count = 0;
+
+            while (temp > 0) {
+                count += (temp & 1);
+                temp >>= 1;
+            }
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+}
