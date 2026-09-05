dfae042e01a8acdb67a7f213379b645ff13a5821
shahil kumar <kumarshahil3456@gmail.com>
2026-09-05 20:02:46 +0530

    Time: 6 ms (45.8%), Space: 126.6 MB (71.37%) - LeetHub

---
+++ Java/Medium/3904-smallest-stable-index-ii/3904-smallest-stable-index-ii-09-05-2026-20-10-21.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+         int n = nums.length;
+        int[] premax = new int[n];
+        premax[0] = nums[0];
+        for(int i = 1; i < n; i++ ){
+            premax[i] = Math.max(premax[i-1], nums[i]);
+        }
+        int[] premin = new int[n];
+        premin[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--){
+            premin[i]=Math.min(premin[i+1], nums[i]);
+        }
+        for(int i = 0;i < n; i++){
+            int ans = premax[i]-premin[i];
+            if(ans <= k){
+                return i;
+            }
+        }
+        return -1;
+    
+    }
+}
