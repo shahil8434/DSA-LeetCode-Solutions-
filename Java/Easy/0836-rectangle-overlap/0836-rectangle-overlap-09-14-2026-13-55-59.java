465922b33e0e28238f9b97656bcdd869c8b97d9b
shahil kumar <kumarshahil3456@gmail.com>
2026-09-14 13:48:17 +0530

    Time: 0 ms (100%), Space: 42.9 MB (17.64%) - LeetHub

---
+++ Java/Easy/0836-rectangle-overlap/0836-rectangle-overlap-09-14-2026-13-55-59.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int x1 = rec1[0];
+        int y1 = rec1[1];
+        int x2 = rec1[2];
+        int y2 = rec1[3];
+
+        int x3 = rec2[0];
+        int y3 = rec2[1];
+        int x4 = rec2[2];
+        int y4 = rec2[3];
+
+        if (x2 <= x3 || x4 <= x1 || y2 <= y3 || y4 <= y1) {
+            return false;
+        }
+
+        return true;
+    }
+}
